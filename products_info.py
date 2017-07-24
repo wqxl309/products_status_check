f95fa3f638bd62f6efa67cecc5b0f025a2e7d494
wqxl309 <wqxl309@hotmail.com>
2017-07-24 19:26:28 +0800

    new kpcmx dir added

---
+++ products_info.py
@@ -0,0 +1,27 @@
+product_info = \
+    [
+        {'product':'BQ1','cttype':'IC','monthtype':'Near1','strategy':'Long','levels':'10','rank':'01','host':r'\\trdvm4'},
+        {'product':'BQ2','cttype':'IC','monthtype':'Near1','strategy':'Long','levels':'10','rank':'01','host':r'\\trdvm3'},
+        {'product':'BQ3','cttype':'IC','monthtype':'Near1','strategy':'Long','levels':'10','rank':'01','host':r'\\trdvm2'},
+        {'product':'JQ1','cttype':'IC','monthtype':'Near1','strategy':'Long','levels':'10','rank':'01','host':r'\\trdvm1'},
+        {'product':'HJ1','cttype':'IC','monthtype':'Near1','strategy':'Long','levels':'10','rank':'01','host':r'\\trdvm2'},
+        {'product':'LS1','cttype':'IC','monthtype':'Near1','strategy':'Long','levels':'10','rank':'01','host':r'\\trdvm3'},
+        {'product':'GD2','cttype':'IC','monthtype':'Near1','strategy':'Long','levels':'10','rank':'01','host':r'\\trdvm1'},
+        {'product':'MS1','cttype':'IC','monthtype':'Near1','strategy':'Long','levels':'10','rank':'01','host':r'\\trdvm4'},
+
+        {'product':'BQ1','cttype':'IF','monthtype':'Near1','strategy':'Long','levels':'10','rank':'01','host':r'\\trdvm5'},
+        {'product':'BQ2','cttype':'IF','monthtype':'Near1','strategy':'Long','levels':'10','rank':'01','host':r'\\trdvm5'},
+        {'product':'BQ3','cttype':'IF','monthtype':'Near1','strategy':'Long','levels':'10','rank':'01','host':r'\\trdvm1'},
+        {'product':'JQ1','cttype':'IF','monthtype':'Near1','strategy':'Long','levels':'10','rank':'01','host':r'\\trdvm4'},
+        {'product':'HJ1','cttype':'IF','monthtype':'Near1','strategy':'Long','levels':'10','rank':'01','host':r'\\trdvm1'},
+        {'product':'LS1','cttype':'IF','monthtype':'Near1','strategy':'Long','levels':'10','rank':'01','host':r'\\trdvm5'},
+        {'product':'GD2','cttype':'IF','monthtype':'Near1','strategy':'Long','levels':'10','rank':'01','host':r'\\trdvm5'},
+
+        {'product':'BQ1','cttype':'IC','monthtype':'Near1','strategy':'Hedge','levels':'10','rank':'01','host':r'\\trdvm1'},
+        {'product':'BQ2','cttype':'IC','monthtype':'Near1','strategy':'Hedge','levels':'10','rank':'01','host':r'\\trdvm3'},
+        {'product':'BQ3','cttype':'IC','monthtype':'Near1','strategy':'Hedge','levels':'10','rank':'01','host':r'\\trdvm2'},
+        {'product':'LS1','cttype':'IC','monthtype':'Near1','strategy':'Hedge','levels':'10','rank':'01','host':r'\\trdvm3'},
+        {'product':'GD2','cttype':'IC','monthtype':'Near1','strategy':'Hedge','levels':'10','rank':'01','host':r'\\trdvm2'},
+
+        {'product':'GD2','cttype':'IC','monthtype':'Back2','strategy':'Hedge','levels':'1','rank':'01','host':r'\\trdvm4'},
+    ]
--- products_kpcmx_check.py
+++ products_kpcmx_check.py
@@ -3,43 +3,21 @@ import datetime as dt
 import pandas as pd
 from checking_functions import *
 
+import products_info as pdinfo
+
 
 if __name__=='__main__':
 
-    kpcmxlist = [
-         'BQ1ICLong',
-         'BQ2ICLong',
-         'BQ3ICLong',
-         'JQ1ICLong',
-         'HJ1ICLong',
-         'GD2ICLong',
-         'LS1ICLong',
-         'MS1ICLong',
-
-         'BQ1IFLong',
-         #'BQ2IFLong',
-         'BQ3IFLong',
-         'JQ1IFLong',
-         'HJ1IFLong',
-         #'GD2IFLong',
-         'LS1IFLong',
-
-        'BQ1ICHedge',
-        'BQ2ICHedge',
-        'BQ3ICHedge',
-        'GD2ICHedge',
-        'LS1ICHedge',
-
-        'GD2yyICHedge'
-    ]
-
-    kpcmx_pool_path = r'E:\take_kpcmx\KPCMX_checking_pool'
-
     kpcmx_report = pd.DataFrame()
-    for p in kpcmxlist:
-        kpcmxdir = os.path.join(kpcmx_pool_path,'_'.join([p,'KPCMX.txt']))
+    for p in pdinfo.product_info:
+        if p['monthtype']=='Back2':
+            yy = 'yy'
+        else:
+            yy = ''
+        tname = ''.join([p['product'],'_',yy,p['cttype'],p['strategy']])
+        pname = tname.replace('_','')
+        kpcmxdir = os.path.join(p['host'],tname,'kpcmx','kpcmx_update','_'.join([pname,'KPCMX.txt']))
         kpcmx = check_kpcmx_status(kpcmxdir)
         kpcmx.update({'kpcmx_filetime': dt.datetime.fromtimestamp(os.path.getmtime(kpcmxdir)).strftime('%Y-%m-%d %H:%M:%S' )})
-        kpcmx_report = kpcmx_report.append(pd.DataFrame(kpcmx,index=[0]),ignore_index=True)
-    kpcmx_report.index = kpcmxlist
+        kpcmx_report = kpcmx_report.append(pd.DataFrame(kpcmx,index=[pname]),ignore_index=False)
     print(kpcmx_report)
